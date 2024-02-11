b41e876319a2543f20924af266226d7c1abc0652
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2024-02-11 21:13:05 +0200

    update ns

--- next.config.js
+++ next.config.js
@@ -1,18 +1,22 @@
-const  {i18n}  = require('./next-i18next.config')
-const path = require('path');
+const { i18n } = require("./next-i18next.config");
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  debug: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === "development",
   // output: 'export',
   // basePath: "/AccLanding",
   compiler: {
     styledComponents: true,
   },
   i18n,
-  localePath: path.resolve('./public/locales'),
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
+  localePath:
+    typeof window === "undefined"
+      ? require("path").resolve("./public/locales")
+      : "/public/locales",
+  ns: ["common","home"],
+  reloadOnPrerender: process.env.NODE_ENV === "development",
 };
 
 module.exports = nextConfig;
