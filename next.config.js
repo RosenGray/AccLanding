bee2c7b9e18a40f7d22fa7cfeb36567e0b62f78a
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2024-02-11 21:08:39 +0200

    vercel locale

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 const  {i18n}  = require('./next-i18next.config')
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  debug: process.env.NODE_ENV === 'development',
   // output: 'export',
   // basePath: "/AccLanding",
   compiler: {
     styledComponents: true,
   },
   i18n,
+  localePath: path.resolve('./public/locales'),
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
 
 module.exports = nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { GetServerSideProps } from "next";
 
 export default function Document(contenxt:any) {
   const {locale} = contenxt
