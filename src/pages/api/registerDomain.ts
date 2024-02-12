3cc57e033a879bd617d2cfbaf128aedc34dfd36e
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2024-02-12 15:42:25 +0200

    options adding

--- src/pages/api/registerDomain.ts
+++ src/pages/api/registerDomain.ts
@@ -6,7 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
+  if (req.method === "POST" || req.method === "OPTIONS") {
     const { domain, created } = req.body;
     try {
       // Connect to the Firestore collection
