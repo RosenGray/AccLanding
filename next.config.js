230691964f82d16832f24642579089a941883edf
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2024-02-12 16:45:24 +0200

    vercel.json

--- next.config.js
+++ next.config.js
@@ -17,13 +17,13 @@ const nextConfig = {
       : "/public/locales",
   ns: ["common","home"],
   reloadOnPrerender: process.env.NODE_ENV === "development",
-  headers: async () => {
+  async headers() {
     return [{
         // matching all API routes
         source:"/api/:path*",
         headers:[
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://www.mako.co.il"  }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
