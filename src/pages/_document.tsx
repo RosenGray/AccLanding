cc9ba04db0cc5a432b67d1caeeb69fc109879fd7
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2024-02-14 09:40:44 +0200

    adding google verification meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-
 export default function Document(contenxt: any) {
   const { locale } = contenxt;
   const dir = locale === "he" ? "rtl" : "ltr";
   return (
     <Html lang={locale ?? "en"} dir={dir}>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="br4ZOGHfWyMgEuCUj1cWsi_xOWpIO_gJ8zl4fGsBHlM"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="language" content={locale} />
         <link rel="icon" href="/favicon.svg" />
